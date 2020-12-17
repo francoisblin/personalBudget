72d5ed96fb322f6d0b6ef32280aa48b1ac8b933f
François Blin <frablin@gmail.com>
2020-12-17 13:41:02 +0100

    Post method populate Array

--- server/db.js
+++ server/db.js
@@ -1,37 +1,46 @@
 const envelopesArray = []
 let idEnvelopes = 1
 
-class Envelope {
-  constructor (name, budget) {
-    this._name = name
-    this._budget = budget
-  }
+// class Envelope {
+//   constructor (name, budget) {
+//     this._name = name
+//     this._budget = budget
+//   }
 
-  get name () {
-    return this._name
-  }
+//   get name () {
+//     return this._name
+//   }
 
-  get budget () {
-    return this._budget
+//   get budget () {
+//     return this._budget
+//   }
+// }
+
+const isValidEnvelope = req => {
+  if (!req.name || typeof req.name !== 'string') {
+    throw new Error('Envelope Name must be a string')
   }
+  if (!isNaN(parseFloat(req.budget)) && isFinite(req.budget)) {
+    req.budget = Number(req.budget)
+  } else {
+    throw new Error('Budget must be a number.')
+  }
+  return true
 }
 
-const envelopeHolidays = new Envelope('Holidays', 12000)
-const envelopeCar = new Envelope('Car', 2500)
+// const envelopeHolidays = new Envelope('Holidays', 12000)
+// const envelopeCar = new Envelope('Car', 2500)
 const addEnvelope = envelope => {
-  const newEnvelope = {
-    id: idEnvelopes,
-    name: envelope.name,
-    budget: envelope.budget
+  if (isValidEnvelope(envelope)) {
+    const newEnvelope = {
+      id: idEnvelopes,
+      name: envelope.name,
+      budget: envelope.budget
+    }
+    envelopesArray.push(newEnvelope)
+    idEnvelopes++
+    return envelopesArray[envelopesArray.length - 1]
   }
-  envelopesArray.push(newEnvelope)
-  idEnvelopes++
-  console.log(envelopesArray)
-  return envelopesArray
 }
 
-addEnvelope(envelopeHolidays)
-addEnvelope(envelopeCar)
-console.log(envelopeHolidays.name)
-
-module.exports = { envelopesArray }
+module.exports = { envelopesArray, addEnvelope }
--- server/envelopes.js
+++ server/envelopes.js
@@ -1,12 +1,20 @@
 const express = require('express')
 const envelopesRouter = express.Router()
+
 module.exports = envelopesRouter
-const { envelopesArray } = require('./db')
+
+const {
+  envelopesArray,
+  addEnvelope
+} = require('./db')
 
 envelopesRouter.get('/', (req, res, next) => {
   return res.send(envelopesArray)
 })
 
 envelopesRouter.post('/', (req, res, next) => {
-  return res.status(201).send()
+  const envelope = req.query
+  console.log(req.query.name)
+  const newEnvelope = addEnvelope(envelope)
+  return res.status(201).send(newEnvelope)
 })
