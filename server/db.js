3c8d5d8490f8dbd797c1bf0f38ff27b3fc1ab706
François Blin <frablin@gmail.com>
2020-12-17 16:05:08 +0100

    add an updating method with POST

--- server/db.js
+++ server/db.js
@@ -1,21 +1,6 @@
 const envelopesArray = []
 let idEnvelopes = 1
 
-// class Envelope {
-//   constructor (name, budget) {
-//     this._name = name
-//     this._budget = budget
-//   }
-
-//   get name () {
-//     return this._name
-//   }
-
-//   get budget () {
-//     return this._budget
-//   }
-// }
-
 const isValidEnvelope = req => {
   if (!req.name || typeof req.name !== 'string') {
     throw new Error('Envelope Name must be a string')
@@ -43,4 +28,20 @@ const addEnvelope = envelope => {
   }
 }
 
-module.exports = { envelopesArray, addEnvelope }
+const getEnvelopeById = id => {
+  const envelope = envelopesArray.find(el => {
+    return el.id === Number(id)
+  })
+  return envelope
+}
+
+const updateBudget = (current, req) => {
+  if (!isNaN(parseFloat(req.budget)) && isFinite(req.budget)) {
+    req.budget = Number(req.budget)
+  } else {
+    throw new Error('Budget must be a number')
+  }
+  current.budget += req.budget
+  return current
+}
+module.exports = { envelopesArray, addEnvelope, getEnvelopeById, updateBudget }
--- server/envelopes.js
+++ server/envelopes.js
@@ -5,8 +5,20 @@ module.exports = envelopesRouter
 
 const {
   envelopesArray,
-  addEnvelope
+  addEnvelope,
+  getEnvelopeById,
+  updateBudget
 } = require('./db')
+
+envelopesRouter.param('envelopeId', (req, res, next, id) => {
+  const envelope = getEnvelopeById(id)
+  if (!envelope) {
+    res.status(404).send()
+  } else {
+    req.envelope = envelope
+    next()
+  }
+})
 
 envelopesRouter.get('/', (req, res, next) => {
   return res.send(envelopesArray)
@@ -14,7 +26,15 @@ envelopesRouter.get('/', (req, res, next) => {
 
 envelopesRouter.post('/', (req, res, next) => {
   const envelope = req.query
-  console.log(req.query.name)
   const newEnvelope = addEnvelope(envelope)
   return res.status(201).send(newEnvelope)
+})
+
+envelopesRouter.get('/:envelopeId', (req, res, next) => {
+  res.send(req.envelope)
+})
+
+envelopesRouter.post('/:envelopeId', (req, res, next) => {
+  const updateEnvelope = updateBudget(req.envelope, req.query)
+  res.send(updateEnvelope)
 })
