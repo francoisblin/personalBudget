cad941f67913f1698dc3588c4aa362b460c8be58
François Blin <frablin@gmail.com>
2020-12-17 12:29:58 +0100

    Creating a local db

--- server/db.js
+++ server/db.js
@@ -1,3 +1,37 @@
 const envelopesArray = []
+let idEnvelopes = 1
+
+class Envelope {
+  constructor (name, budget) {
+    this._name = name
+    this._budget = budget
+  }
+
+  get name () {
+    return this._name
+  }
+
+  get budget () {
+    return this._budget
+  }
+}
+
+const envelopeHolidays = new Envelope('Holidays', 12000)
+const envelopeCar = new Envelope('Car', 2500)
+const addEnvelope = envelope => {
+  const newEnvelope = {
+    id: idEnvelopes,
+    name: envelope.name,
+    budget: envelope.budget
+  }
+  envelopesArray.push(newEnvelope)
+  idEnvelopes++
+  console.log(envelopesArray)
+  return envelopesArray
+}
+
+addEnvelope(envelopeHolidays)
+addEnvelope(envelopeCar)
+console.log(envelopeHolidays.name)
 
 module.exports = { envelopesArray }
